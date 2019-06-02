b1b98712cb067566810080ba210cbcd934e4ddc0
Eugene Shishlannikov <sbarakad@yahoo.com>
2019-06-01 21:45:04 -0300

    1.Resources release on error added. 2.Response implemented. 3. Run in terminal with HTTPServerDaemon.exe&, use curl 127.0.0.1/some-request/<whatever string here> to invoke

--- src/httpServer.c
+++ src/httpServer.c
@@ -2,17 +2,20 @@
 /* INCLUDES *******************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <libsoup/soup.h>
 #include <assert.h>
+
 
 
 /* CONSTANTS ******************************/
 #define PORT_NUMBER 2222
 #define SERVER_CREATION_ERR_MSG "Failed to create HTTP server\n"
 #define SERVER_HEADER_STR "HTTP server daemon"
-#define LISTEN_PORT_BOUND_ERR_MSG "Port could not be bound or other error occurred.\n"
-#define PATH "/"
-#define SUCCESS_MSG "\n127.0.0.1:2222 was browsed"
+#define LISTEN_PORT_BOUND_ERR_MSG "Port could not be bound or other error occurred. Port number: %d\n"
+#define PATH "/some-request"
+#define URL_DELIMITER "/"
+#define MIME_CONTENT_TYPE "text/html"
 
 
 
@@ -39,12 +42,15 @@ int main(void) {
 	if(NULL == soupServer)
 	{
 		g_print(SERVER_CREATION_ERR_MSG);
+		return EXIT_FAILURE;
 	}
 
 	ssllResult = soup_server_listen_local(soupServer, PORT_NUMBER, 0,gError);
 	if(FALSE == ssllResult)
 	{
-		g_print(LISTEN_PORT_BOUND_ERR_MSG);
+		g_print(LISTEN_PORT_BOUND_ERR_MSG, PORT_NUMBER);
+		soup_server_disconnect(soupServer);
+		return EXIT_FAILURE;
 	}
 
 	soup_server_add_handler(soupServer, PATH, callback, NULL, NULL);
@@ -64,5 +70,20 @@ void callback(SoupServer *soupServer,
 			  SoupClientContext *client,
 			  gpointer user_data)
 {
-	g_print(SUCCESS_MSG);
+	char* token;
+	guint32 respBodyLen;
+	gpointer respBody;
+
+	token = strtok((char*)path, URL_DELIMITER);
+	token = strtok(NULL, URL_DELIMITER);
+	if(NULL == token)
+	{
+		token = URL_DELIMITER;
+	}
+
+	respBodyLen = strlen(token) + 1;
+	respBody = g_memdup(token, respBodyLen);
+
+	soup_message_set_status(msg, SOUP_STATUS_OK);
+	soup_message_set_response(msg, MIME_CONTENT_TYPE, SOUP_MEMORY_TAKE, respBody, respBodyLen);
 }
